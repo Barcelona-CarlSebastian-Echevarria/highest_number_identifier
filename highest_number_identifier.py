402b7aedd0784af44e06aa2470d1d017ba4167d0
Carl <carlsebpogi123@gmail.com>
2024-10-15 11:16:43 +0800

    Fixed the line spacing for better visual appearance

--- highest_number_identifier.py
+++ highest_number_identifier.py
@@ -22,8 +22,6 @@ def prompt(numerical_input):
             except ValueError: 
                print("Please enter a number")
 
-
-
 # Assigns the returned value to a variable
 def variable_assignment():
     print("Press 'q' to quit (Note: The program is not case-sensitive)")
@@ -34,28 +32,23 @@ def variable_assignment():
     user_input5 = prompt("Enter a fifth number: ")
     return user_input1, user_input2, user_input3, user_input4, user_input5
 
-
 # Finds the highest number by simultaneously comparing the numbers at 1st and 2nd place and 3rd and 4th, then compares it to the 5th
 def find_the_highest_number(user_input1, user_input2, user_input3, user_input4, user_input5):
     if user_input1 > user_input2:
         result1 = user_input1
-
     else:
         result1 = user_input2
-
 
     if user_input3 > user_input4:
         result2 = user_input3
     else:
         result2 = user_input4
 
-
     if result1 > result2:
         if result1 > user_input5:
             return result1
         else:
             return user_input5
-
     else:
         if result2 > user_input5:
             return result2
